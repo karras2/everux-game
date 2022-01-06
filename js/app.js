649c5ab741e857e93236b8e7928706d92a2c7548
King <68120448+karras2@users.noreply.github.com>
2022-01-06 18:56:58 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4606,8 +4606,8 @@ const gameDraw = (() => {
 						picture = getEntityImageFromMockup(index, 10),
 						position = mockups[index].position,
 						scale = (0.8 * size) / position.axis,
-						xx = ax + 0.5 * size - scale * position.middle.x * Math.cos(angle),
-						yy = ay + 0.5 * size - scale * position.middle.x * Math.sin(angle);
+						xx = ax + 0.5 * size - scale * position.middle.x * Math.cos(upgradeSpin),
+						yy = ay + 0.5 * size - scale * position.middle.x * Math.sin(upgradeSpin);
 					drawEntity(xx, yy, picture, 0.5, 1, (scale / picture.size) * 2, upgradeSpin, !0);
 					ctx.strokeStyle = color.black;
 					ctx.globalAlpha = 1;
